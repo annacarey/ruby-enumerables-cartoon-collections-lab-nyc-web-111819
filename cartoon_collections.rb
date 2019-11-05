631500e9f90afb320433df086165a462c7e710fd
declarative-blog-8005 <anna@annajcarey.com>
2019-11-05 00:57:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find_all
+  array.find_all do |cheese|
+    cheese_types.contains
 end
